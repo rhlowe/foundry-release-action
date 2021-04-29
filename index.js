ed4f254082fb20a1a2e153cbac17b1e0e32687a3
Tim White <twhite@fool.com>
2021-04-29 17:00:25 -0600

    Starting on version replace

--- index.js
+++ index.js
@@ -10,15 +10,20 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  //console.log(`The event payload: ${payload}`);
 
-  fs.readFile("./system.json", 'utf8', function (err,data) {
+  fs.readFile("system.json", 'utf8', function (err,data) {
+    console.log("system.json before replace");
     console.log(data);
     let formatted = data.replace(/VERSION/g, '0.1');
 
-    fs.writeFile("./system.json", formatted, 'utf8', function (err) {
+    fs.writeFile("system.json", formatted, 'utf8', function (err) {
       if (err) return console.log(err);
     });
+  });
+  fs.readFile("system.json", 'utf8', function (err,data) {
+    console.log("system.json after replace");
+    console.log(data);
   });
 } catch (error) {
   core.setFailed(error.message);
