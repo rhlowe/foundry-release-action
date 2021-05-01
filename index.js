7a4441f5c4843efab3ce4d78eb30b505501c4ebd
Tim White <twhite@fool.com>
2021-05-01 07:42:30 -0600

    Starting on version replace

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const shell = require('shelljs')
 const fs = require('fs')
+const actionToken = core.getInput('actionToken')
+const octokit = github.getOctokit(actionToken)
+const owner = github.context.payload.repository.owner.login
+const repo = github.context.payload.repository.name
 
 async function createRelease () {
   // Create Release
-  const actionToken = core.getInput('actionToken')
-  const octokit = github.getOctokit(actionToken)
-
   try {
     const createReleaseResponse = await octokit.rest.repos.createRelease({
-      owner: github.context.payload.repository.owner.login,
-      repo: github.context.payload.repository.name,
+      owner: owner,
+      repo: repo,
       tag_name: 'v0.0',
-      name: 'test release',
+      name: `test release ${Date.now()}`,
       body: 'test release',
       draft: true,
       prerelease: true
     })
 
     console.log(createReleaseResponse)
+    return createReleaseResponse
+  } catch
+    (error) {
+    core.setFailed(error.message)
+  }
+}
+
+async function addZip (createReleaseResponse) {
+  try {
+    const filePath = 'latest.zip'
+    const data = fs.readFileSync(filePath)
+
+    // Determine content-length for header to upload asset
+    const contentLength = filePath => fs.statSync(filePath).size
+
+    const headers = { 'content-type': 'application/zip', 'content-length': contentLength }
+
+    const uploadAssetResponse = await octokit.rest.repos.uploadReleaseAsset({
+      url: createReleaseResponse.data.upload_url,
+      headers,
+      name: filePath,
+      file: data
+    })
+
+    console.log(uploadAssetResponse)
   } catch
     (error) {
     core.setFailed(error.message)
@@ -35,7 +62,10 @@ try {
   let formatted = data.replace(/{{VERSION}}/g, '0.1')
   fs.writeFileSync('system.json', formatted, 'utf8')
 
-  createRelease()
+  const releaseResponse = createRelease()
+  shell.exec('git commit -am "release"')
+  shell.exec('git archive -o latest.zip')
+  const uploadResponse = addZip()
 
 } catch
   (error) {
